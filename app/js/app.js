2d03cec400d078ba3ad9e4eedb043ece7938547e
Wei Liang <weiliang.c3@gmail.com>
2016-07-16 01:36:51 +0800

    added search feedback

--- app/js/app.js
+++ app/js/app.js
@@ -36087,20 +36087,26 @@ function MainController($http, $state, $stateParams, API){
   self.searchInput = null;
   self.searchOrder = "desc";
   self.error = null;
+  self.message = null;
   self.searchResults = null;
+  self.searchPageNo = 1;
 
   self.search = search;
 
 
   function search(){
     console.log("searchinput", self.searchInput);
+    self.message = "Searching...";
 
     $http.get( API + "search/users", {
       params: {
         q: self.searchInput,
         order: self.searchOrder,
+        page: self.searchPageNo,
+        per_page: 30
       },
     }).then(function(res){
+      self.message = null;
       console.log("firstres.data", res.data);
       self.searchResults = res.data;
       self.error = null;
@@ -36108,6 +36114,7 @@ function MainController($http, $state, $stateParams, API){
         self.error = "Your search yielded no results";
       }
     }, function(res){
+      self.message = null;
       console.log("error res", res);
       self.error = res.data;
     });
--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -10,20 +10,26 @@ function MainController($http, $state, $stateParams, API){
   self.searchInput = null;
   self.searchOrder = "desc";
   self.error = null;
+  self.message = null;
   self.searchResults = null;
+  self.searchPageNo = 1;
 
   self.search = search;
 
 
   function search(){
     console.log("searchinput", self.searchInput);
+    self.message = "Searching...";
 
     $http.get( API + "search/users", {
       params: {
         q: self.searchInput,
         order: self.searchOrder,
+        page: self.searchPageNo,
+        per_page: 30
       },
     }).then(function(res){
+      self.message = null;
       console.log("firstres.data", res.data);
       self.searchResults = res.data;
       self.error = null;
@@ -31,6 +37,7 @@ function MainController($http, $state, $stateParams, API){
         self.error = "Your search yielded no results";
       }
     }, function(res){
+      self.message = null;
       console.log("error res", res);
       self.error = res.data;
     });
